44290d6928e5bc9aa4567fc4ea098d8e82dd4114
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2025-09-11 23:09:55 +0400

    task1.cpp

--- EVN-2/task1.cpp
+++ EVN-2/task1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
 using namespace std;
+
+int myMax(int* arr, int n) {
+    int max_el = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_el) {
+            max_el = arr[i];
+        }
+    }
+    return max_el;
+}
+
+int myMin(int* arr, int n) {
+    int min_el = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min_el) {
+            min_el = arr[i];
+        }
+    }
+    return min_el;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -8,19 +28,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int c = atoi(argv[3]);
+    int arr[3];
+    for (int i = 0; i < 3; i++) {
+        try {
+            size_t pos;
+            arr[i] = stoi(argv[i + 1], &pos);
+            if (pos != string(argv[i + 1]).size()) {
+                cerr << "Ошибка: '" << argv[i + 1] << "' не является числом\n";
+                return 1;
+            }
+        } catch (...) {
+            cerr << "Ошибка: '" << argv[i + 1] << "' не является числом\n";
+            return 1;
+        }
+    }
 
-    int minVal = a;
-    if (b < minVal) minVal = b;
-    if (c < minVal) minVal = c;
-
-    int maxVal = a;
-    if (b > maxVal) maxVal = b;
-    if (c > maxVal) maxVal = c;
-
-    cout << "min-" << minVal << ", max-" << maxVal << endl;
-
+    cout << "min-" << myMin(arr, 3) << ", max-" << myMax(arr, 3) << endl;
     return 0;
 }
+
+//решила вообще заново написать весь код))
