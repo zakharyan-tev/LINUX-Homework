4a676f85872034fecd4cb6cf55bd718e507d96f8
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2025-09-28 21:06:39 +0400

    Update read-file.cpp

--- FS/FS-1/read-file.cpp
+++ FS/FS-1/read-file.cpp
@@ -1,29 +1,39 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <fcntl.h>   
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <stdio.h>  
 
 int main(int argc, char** argv) {
-	if(argc != 2) {
-        perror("Wrong args");
+    if (argc != 2) {
+        perror("Wrong arguments");
         exit(EXIT_FAILURE);
     }
 
     const char* filePath = argv[1];
-
-    std::ifstream file(filePath, std::ios::in | std::ios::binary);
-    if (!file) {
-        std::cerr << "Error: Cannot open file " << filePath << std::endl;
-        return 1;
+	
+    int fd = open(filePath, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
 
     const size_t bufferSize = 1024;
     char buffer[bufferSize];
+    ssize_t bytesRead;
 
-    while (file.read(buffer, bufferSize) || file.gcount() > 0) {
-        std::cout.write(buffer, file.gcount());
+    while ((bytesRead = read(fd, buffer, bufferSize)) > 0) {
+        ssize_t bytesWritten = write(1, buffer, bytesRead);
+        if (bytesWritten == -1) {
+            perror("Error writing to stdout");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    file.close();
-    return 0; 
+    if (bytesRead == -1) {
+        perror("Error reading file");
+    }
+
+    close(fd);
+    return 0;
 }
